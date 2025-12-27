efa790c0ec91c4349c3f88c830d7b387810a9b35
lilinzhi <daphnelaw@163.com>
2025-12-28 00:12:52 +0100

    fix the pv

--- core/src/de/tum/cit/fop/maze/screen/MazeGame1.java
+++ core/src/de/tum/cit/fop/maze/screen/MazeGame1.java
@@ -1,4 +1,15 @@
 package de.tum.cit.fop.maze.screen;
 
 public class MazeGame1 {
+    public enum GameResult{
+        SUCCESS,
+        FAILURE_DEAD,
+        FAILURE_LOST,
+        RUNNING
+    }
+    private GameResult result=GameResult.RUNNING;
+    //TODO 判断是否再限时 逃出去了，如果死了就是pv6，没逃出去就是pv7，成功就进入做梦pv，解锁下一章节
+
+
+
 }
