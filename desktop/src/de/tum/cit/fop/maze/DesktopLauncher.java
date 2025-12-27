ec01008c525f8bb43cafd0f63b240dec4d62fc01
IdaPG <allewesin@gmail.com>
2025-12-27 23:00:19 +0100

    door effect first realization

--- desktop/src/de/tum/cit/fop/maze/DesktopLauncher.java
+++ desktop/src/de/tum/cit/fop/maze/DesktopLauncher.java
@@ -3,7 +3,6 @@ package de.tum.cit.fop.maze;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-
 import games.spooky.gdx.nativefilechooser.desktop.DesktopFileChooser;
 
 /**
