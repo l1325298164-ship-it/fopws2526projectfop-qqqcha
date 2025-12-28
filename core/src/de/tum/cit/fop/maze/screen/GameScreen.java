3c5802b062a39be1bac145accea1581a8b656018
lilinzhi <daphnelaw@163.com>
2025-12-28 23:57:48 +0100

    pack

--- core/src/de/tum/cit/fop/maze/screen/GameScreen.java
+++ core/src/de/tum/cit/fop/maze/screen/GameScreen.java
@@ -278,6 +278,7 @@ public class GameScreen implements Screen {
                 continue;
             }
 
+
             boolean isFront =
                     mazeRenderer.isWallInFrontOfAnyEntity(
                             group.startX,
