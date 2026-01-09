6c8db7cc909395a6faf1537b3f6f28bb806564ee
Ida P <allewesin@gmail.com>
2026-01-10 00:54:32 +0100

    sk

--- core/src/de/tum/cit/fop/maze/screen/LeaderboardScreen.java
+++ core/src/de/tum/cit/fop/maze/screen/LeaderboardScreen.java
@@ -2,6 +2,7 @@ package de.tum.cit.fop.maze.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
